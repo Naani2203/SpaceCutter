8b45de8bb7e79239c4942a0c405fd15e7ef69a28
Naani <snstheja@gmail.com>
2020-08-15 13:09:42 -0700

    Unity Project Created

---
+++ SpaceCutter_Project/Assets/Scripts/GeneratedMesh.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratedMesh 
+{
+    private List<Vector3> _Vertices = new List<Vector3>();
+    private List<Vector3> _Normals = new List<Vector3>();
+    private List<Vector2> _UVs = new List<Vector2>();
+    private List<List<int>> _SubMeshIndices = new List<List<int>>();
+
+    public List<Vector3> Vertices { get { return _Vertices; } set { _Vertices = value; } }
+    public List<Vector3> Normals { get { return _Normals; } set { _Normals = value; } }
+    public List<Vector2> UVs { get { return _UVs; } set { _UVs = value; } }
+    public List<List<int>> SubMeshIndices {  get { return _SubMeshIndices; } set { _SubMeshIndices = value; } }
+
+    public void AddTriangle(MeshTriangle triangle)
+    {
+        int currentVerticeCount = _Vertices.Count;
+
+        _Vertices.AddRange(triangle.Vertices);
+        _Normals.AddRange(triangle.Normals);
+        _UVs.AddRange(triangle.UVs);
+
+        if(_SubMeshIndices.Count < triangle.SubMeshIndex + 1)
+        {
+            for(int i = _SubMeshIndices.Count; i<triangle.SubMeshIndex + 1; i++)
+            {
+                _SubMeshIndices.Add(new List<int>());
+            }
+        }
+
+        for(int i = 0; i < 3; i++)
+        {
+            _SubMeshIndices[triangle.SubMeshIndex].Add(currentVerticeCount + i);
+        }
+    }
+}
---
+++ SpaceCutter_Project/Assets/Scripts/MeshTriangle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshTriangle
+{
+    private List<Vector3> _Vertices = new List<Vector3>();
+    private List<Vector3> _Normals = new List<Vector3>();
+    private List<Vector2> _UVs = new List<Vector2>();
+    int _SubMeshIndex;
+
+    public List<Vector3> Vertices { get { return _Vertices; } set { _Vertices = value; } }
+    public List<Vector3> Normals { get { return _Normals; } set { _Normals = value; } }
+    public List<Vector2> UVs { get { return _UVs; } set { _UVs = value; } }
+    public int SubMeshIndex { get { return _SubMeshIndex; } set { SubMeshIndex = value; } }
+
+    public MeshTriangle(Vector3[] vertices, Vector3[] normals, Vector2[] uvs, int submeshindex)
+    {
+        Clear();
+        
+        _Vertices.AddRange(vertices);
+        _Normals.AddRange(normals);
+        _UVs.AddRange(uvs);
+
+        _SubMeshIndex = submeshindex;
+    }
+
+    public void Clear()
+    {
+        _Vertices.Clear();
+        _Normals.Clear();
+        _UVs.Clear();
+
+        _SubMeshIndex = 0;
+    }
+}
