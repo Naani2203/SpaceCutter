c6cf9ec9c65c9804a5f4980acec563578193e0bf
Naani <snstheja@gmail.com>
2020-08-15 23:17:19 -0700

    added muzzle flash and fixed cut angle

--- SpaceCutter_Project/Assets/Scripts/DrawCut.cs
+++ SpaceCutter_Project/Assets/Scripts/DrawCut.cs
@@ -12,44 +12,76 @@ public class DrawCut : MonoBehaviour
     public GameObject obj;
     public Material Mat;
     public  bool IsVertical;
+    [SerializeField]
+    private GameObject _VertAim;
+    [SerializeField]
+    private GameObject _HorAim;
+    [SerializeField]
+    private ParticleSystem _ParticleSystem;
 
-    void Start() {
+    void Start() 
+    {
         cam = GetComponent<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+        if (!IsVertical)
+        {
+            IsVertical = true;
+            _VertAim.SetActive(true);
+            _HorAim.SetActive(false);
+        }
+        else
+        {
+            IsVertical = false;
+            _VertAim.SetActive(false);
+            _HorAim.SetActive(true);
+        }
     }
 
     void Update()
     {
+        
         Vector3 mouse = Input.mousePosition;
+        var x = Screen.width / 2;
+        var y = Screen.height / 2;
         mouse.z = -cam.transform.position.z;
         if (Input.GetMouseButtonDown(0)) 
         {
             RaycastHit hitInfo = new RaycastHit();
-            bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
+            bool hit = Physics.Raycast( cam.transform.position, cam.transform.forward, out hitInfo);
             if (hit)
             {                
                 if (hitInfo.transform.gameObject.tag == "Cut")
                 {
                     obj = hitInfo.transform.gameObject;
+                    _ParticleSystem.Play();
                 }
 
                 pointA = cam.ScreenToWorldPoint(mouse);
                 if (IsVertical)
                 {
-                    pointB = new Vector3(pointA.x, pointA.y + 2, pointA.z);
+                    pointB = new Vector3(pointA.x, pointA.y , pointA.z) + transform.up*2;
                 }
                 else
                 {
-                    pointB = new Vector3(pointA.x + 2, pointA.y, pointA.z);
+                    pointB = new Vector3(pointA.x, pointA.y, pointA.z) +transform.right *2;
                 }
-            }
                 CreateSlicePlane();
+            }
         }
         if(Input.GetKeyDown(KeyCode.T))
         {
             if (!IsVertical)
+            {
                 IsVertical = true;
+                _VertAim.SetActive(true);
+                _HorAim.SetActive(false);
+            }
             else
+            {
                 IsVertical = false;
+                _VertAim.SetActive(false);
+                _HorAim.SetActive(true);
+            }
         }
 
        
--- SpaceCutter_Project/Assets/Scripts/PlayerController.cs
+++ SpaceCutter_Project/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -8,7 +9,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float _MouseSensitivity;
     [SerializeField]
-    private Transform _Camera;
+    private Camera _Camera;
     private float _MouseX;
     private float _MouseY;
     private float _XRot;
@@ -20,27 +21,66 @@ public class PlayerController : MonoBehaviour
     private float _Force;
     [SerializeField]
     private float _MaxVelocity;
+    private float _Horizontal;
+    private float _Vertical;
+    private float _Thrusters;
+    private bool _Brake;
+    private float _Roll;
+    private Vector3 _Direction;
+    private Vector3 _CameraDirection;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         LookWithMouse();
+        PlayerMovementInput();
+    }
+    void FixedUpdate()
+    {
+        ApplyPlayerPhysics();
     }
 
     private void LookWithMouse()
     {
+        Vector3 mouse = Input.mousePosition;
+        _CameraDirection = _Camera.ScreenToWorldPoint(mouse);
         _MouseX = Input.GetAxis("Mouse X") * _MouseSensitivity * Time.deltaTime;
         _MouseY = Input.GetAxis("Mouse Y") * _MouseSensitivity * Time.deltaTime;
-        transform.Rotate(Vector3.up * _MouseX);
-        _XRot -= _MouseY;
-        _XRot = Mathf.Clamp(_XRot, -90, 90);
-        _Camera.localRotation = Quaternion.Euler(_XRot, 0, 0);
 
+        //transform.rotation = (Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(_CameraDirection),0.01f));
+
+        transform.Rotate(0, Input.GetAxis("Mouse X") * _MouseSensitivity * Time.deltaTime, 0);
+        transform.Rotate(-Input.GetAxis("Mouse Y") * _MouseSensitivity *Time.deltaTime, 0, 0);
+        transform.Rotate(0, 0, -Input.GetAxis("Roll") * 45 * Time.deltaTime);
+
+
+        //transform.Rotate(Vector3.up * _MouseX);
+        //_XRot -= _MouseY;
+        //_XRot = Mathf.Clamp(_XRot, -90, 90);
+        //_Camera.transform.localRotation = Quaternion.Euler(_XRot, _MouseX, 0);
+        //transform.LookAt(_Camera.transform.forward);
+    }
+
+    private void PlayerMovementInput()
+    {
+        _Horizontal = Input.GetAxis("Horizontal");
+        _Vertical = Input.GetAxis("Vertical");
+        _Thrusters = Input.GetAxis("Jump");
+        _Brake = Input.GetKey(KeyCode.LeftControl);
+        _Roll = Input.GetAxis("Roll");
+    }
+
+    private void ApplyPlayerPhysics()
+    {
+        if(_RB.velocity.magnitude<_MaxVelocity)
+        {
+            _Direction = new Vector3(_Horizontal, _Thrusters, _Vertical);
+            _RB.AddForce(transform.right*_Horizontal * _Force);
+            _RB.AddForce(transform.up * _Thrusters * _Force);
+            _RB.AddForce(transform.forward * _Vertical * _Force);
+        }
+        if(_Brake)
+        {
+            _RB.velocity *= 0.9f;
+        }
     }
 }
